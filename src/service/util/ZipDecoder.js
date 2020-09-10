cb4af3337520e5ab3cfb3258eb1995ced4063a22
ryunosinfx <ryunosinfxxul@gmail.com>
2020-09-11 03:26:56 +0900

    デバッグ分追加

--- src/service/util/ZipDecoder.js
+++ src/service/util/ZipDecoder.js
@@ -5,17 +5,19 @@ export class ZipDecoder {
 	static decode(dURI) {
 		const ab = Base64Util.dataURI2bs(dURI);
 		const retObj = {};
+		console.log(ab);
 		try {
 			const u8a = new Uint8Array(ab);
 			const unzip = new Zlib.Unzip(u8a);
 			const importFileList = unzip.getFilenames();
+			console.log(importFileList);
 
 			for (let filePath of importFileList) {
 				const bs = Base64Util.u8a2bs(unzip.decompress(filePath));
 				retObj[filePath] = bs;
 			}
 		} catch (e) {
-			console.log(e.message);
+			console.warn(e.message);
 		}
 		console.log('ZipDecoder retObj');
 		console.log(retObj);
