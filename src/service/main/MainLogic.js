0dc29a0609b400b600aeca3306590a136d9f57bb
ryunosinfx <ryunosinfxxul@gmail.com>
2020-09-23 22:54:07 +0900

    デバッグ文追加

--- src/service/main/MainLogic.js
+++ src/service/main/MainLogic.js
@@ -33,7 +33,7 @@ export class MainLogic {
 		const blob = await PostMessager.postToParent({ path });
 		const decoded = ZipDecoder.decode(blob);
 		const bin = decoded['export.bin'];
-		console.log('----decoded---getZip');
+		console.log('----decoded---getZip bin:' + bin);
 		console.log(decoded);
 		const hash = bin ? bin.hash : null;
 		const file = bin ? this.ExporsFileDecoder.decode(bin.u8a) : {};
