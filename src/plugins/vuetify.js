b60ae1456cb304613f5196478f91a27f7c9dc67c
ryunosinfx <ryunosinfxxul@gmail.com>
2020-09-27 18:41:09 +0900

    れ

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import colors from 'vuetify/lib/util/colors';
+
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 
-const opts = {};
+const opts = {
+	theme: {
+		themes: {
+			light: {
+				primary: colors.red.darken1, // #E53935
+				secondary: colors.red.lighten4, // #FFCDD2
+				accent: colors.indigo.base, // #3F51B5
+			},
+		},
+	},
+};
 
 export default new Vuetify(opts);
