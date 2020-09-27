7c0c119d3500d6ca55cd0a4619a3e5d4fcb19e2e
ryunosinfx <ryunosinfxxul@gmail.com>
2020-09-28 03:21:38 +0900

    Webpackの改行、コメント有りminifyから抜き出す対応。※暫定対応

--- src/service/util/BookmarkletBuilder.js
+++ src/service/util/BookmarkletBuilder.js
@@ -1,10 +1,23 @@
+const SLASH = '&#47;';
+const QUOTE = '&quot;';
 export class BookmarkletBuilder {
 	static build(src) {
-		const bookmarklet = src
+		const rows = src
 			.split('\t')
 			.join('')
-			.split('\n')
-			.join('');
+			.replace(/\/\*[^\/]+\*\//g, '')
+			.split('\n');
+		const filterd = [];
+		for (let row of rows) {
+			const newRow = row.replace(/http:\/\//g, 'http:' + SLASH + SLASH).replace(/https:\/\//g, 'https:' + SLASH + SLASH);
+			filterd.push(
+				newRow
+					.split('//')[0]
+					.split(SLASH)
+					.join('/')
+			);
+		}
+		const bookmarklet = filterd.join(' ');
 		return `javascript:(()=>{${bookmarklet}})()`;
 	}
 }
